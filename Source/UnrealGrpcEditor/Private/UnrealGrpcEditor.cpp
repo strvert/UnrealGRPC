34fd90982b69866cef111f3671c47e756b6b67cb
Riku Ishikawa <strv@strv.dev>
2023-06-12 10:46:48 +0900

    feat: プラグインコードの雛形を追加

---
+++ Source/UnrealGrpcEditor/Private/UnrealGrpcEditor.cpp
@@ -0,0 +1,39 @@
+﻿#include "UnrealGrpcEditor.h"
+
+#include "ToolMenus.h"
+
+#define LOCTEXT_NAMESPACE "FUnrealGrpcEditorModule"
+
+void FUnrealGrpcEditorModule::StartupModule()
+{
+	// FCoreDelegates::OnPostEngineInit.AddRaw(this, &FUnrealGrpcEditorModule::RegisterToolMenu);
+	RegisterToolMenu();
+}
+
+void FUnrealGrpcEditorModule::ShutdownModule()
+{
+    
+}
+
+void FUnrealGrpcEditorModule::RegisterToolMenu()
+{
+	if (UToolMenus* ToolMenus = UToolMenus::Get())
+	{
+		UToolMenu* Menu = ToolMenus->ExtendMenu("LevelEditor.MainMenu.Tools");
+		FToolMenuSection Section = Menu->AddSection("UnrealGrpc", LOCTEXT("UnrealGrpc", "UnrealGrpc"));
+
+		Section.AddMenuEntry(
+			"GRPCClientGenerator",
+			LOCTEXT("GRPCClientGenerator", "gRPC Client Generator"),
+			LOCTEXT("GRPCClientGeneratorTooltip", "Generate gRPC client code from proto files"),
+			FSlateIcon(),
+			FUIAction(FExecuteAction::CreateLambda([]() {
+				UE_LOG(LogTemp, Warning, TEXT("UnrealGrpc"));
+			}))
+		);
+	}
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FUnrealGrpcEditorModule, UnrealGrpcEditor)
---
+++ Source/UnrealGrpcEditor/UnrealGrpcEditor.Build.cs
@@ -0,0 +1,27 @@
+﻿using UnrealBuildTool;
+
+public class UnrealGrpcEditor : ModuleRules
+{
+    public UnrealGrpcEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "ToolMenus"
+            }
+        );
+    }
+}
